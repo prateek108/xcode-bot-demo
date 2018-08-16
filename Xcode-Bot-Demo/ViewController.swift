e2de36275ccb483e59db670e1b4262c8c9644ee5
Sushant Jagtap <sushant@giantleapsystems.com>
2018-08-16 17:56:11 +0530

    new push

---
+++ Xcode-Bot-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Xcode-Bot-Demo
+//
+//  Created by Tushar Waghmare on 16/08/18.
+//  Copyright © 2018 GiantLeapSystems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
